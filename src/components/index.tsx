2a31979725ffba4e1622a9c10f28c7fe1e8ab405
Radiant-F <radiantfadilah0@gmail.com>
2023-09-28 15:53:21 +0700

    import presence scanner components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,7 @@
 import Background from './screens/Background';
 import Gap from './screens/Gap';
 import Presence from './screens/home/Presence';
+import ModalPermission from './screens/presence-scanner/ModalPermission';
+import Scanner from './screens/presence-scanner/Scanner';
 
-export {Background, Gap, Presence};
+export {Background, Gap, Presence, Scanner, ModalPermission};
